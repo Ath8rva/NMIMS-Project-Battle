1899ba56e449f44907c16c3e34a388fd3e0a096d
Ath8rva <96336561+Ath8rva@users.noreply.github.com>
2021-12-18 17:46:04 +0530

    Add files via upload

---
+++ Atharva Rajale Submission.c
@@ -0,0 +1,205 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("Atharva Rajale's entry for NMIMS Project Battle 2021\n");
+    printf("52 Cases in a program about 52\n");
+    printf("Choose which way you want to see '52' displayed: (52 Cases)\n");
+    printf("1. ASCII Art for 52\n");
+    printf("2. Mathematical properties of 52\n");
+    printf("3. ASCII Art for Roman Numerals of 52\n");
+    printf("4. Fun Fact about 52\n");
+    printf("5. Not so fun fact about 52\n");
+    printf("I couldn't complete all 52 cases due to the time constraint (mostly cause I lack creativity :P), so type in any number from 6 to 52 for an inspirational quote :) \n");
+    scanf("%d",&n);
+    switch(n)
+        {
+            case 1:
+                printf("55555555   22222\n");
+                printf("5         2     2\n");
+                printf("5              2\n");
+                printf("5555555       2\n");
+                printf("       5     2 \n");
+                printf("       5    2\n");
+                printf("5555555    22222222\n");
+                printf("Is this good art? Probably not, but atleast it's hand made\n");
+                break;
+            case 2:
+                printf("Is 52 a prime number?	No\n");
+                printf("Is 52 a perfect number?	No (52 is perfect to us tho)\n");
+                printf("Number of divisors	    6\n");
+                printf("List of dividers	    1, 2, 4, 13, 26, 52\n");
+                printf("Sum of divisors	        98\n");
+                printf("Prime factorization	    2^2 x 13\n");
+                printf("Prime factors	        2, 13\n");
+                break;
+            case 3:
+                printf("L            I I I I I I I    I I I I I I I\n");
+                printf("L                  I                I\n");
+                printf("L                  I                I\n");
+                printf("L                  I                I\n");
+                printf("L                  I                I\n");
+                printf("L                  I                I\n");
+                printf("L L L L L    I I I I I I I    I I I I I I I\n");
+                break;
+            case 4:
+                printf("The number of playing cards in a normal deck is 52, how convenient! Here, have some more ASCII art\n");
+                printf("          _____\n");
+                printf("         |A .  | _____\n");
+                printf("         | /.| ||A ^  | _____\n");
+                printf("         |(_._)|| / | ||A _  | _____\n");
+                printf("ejm98    |  |  || | / || ( ) ||A_ _ |\n");
+                printf("         |____V||  .  ||(_'_)||( v )|\n");
+                printf("                |____V||  |  || | / |\n");
+                printf("                       |____V||  .  |\n");
+                printf("                              |____V|\n");
+                break;
+            case 5:
+                printf("52 (fifty-two) is the natural number following 51 and preceding 53.\n");
+                break;
+            case 6:
+                printf("“The past can hurt. But the way I see it, you can either run from it or learn from it.” – Rafiki, The Lion King\n");
+                break;
+            case 7:
+                printf("“No matter how your heart is grieving, if you keep on believing, the dream that you wish will come true.” – Cinderella, Cinderella\n");
+                break;
+            case 8:
+                printf("“Don’t think and don’t worry. When the times comes, you’ll know what to do.”  – Helen Parr, The Incredibles\n");
+                break;
+            case 9:
+                printf("“If you don’t think, then you shouldn’t talk.”  – March Hare,  Alice in Wonderland\n");
+                break;
+            case 10:
+                printf("“You just need to believe in yourself.” – Rex, Toy Story 2\n");
+                break;
+            case 11:
+                printf("“Sometimes the right path is not the easiest one.” – Grandmother Willow, Pocahontas\n");
+                break;
+            case 12:
+                printf("“The only thing predictable about life is its unpredictability.” – Remy, Ratatouille\n");
+                break;
+            case 13:
+                printf("“If watching is all you’re going to do, then you’re going to watch your life go by without you.” – Laverne, The Hunchback of Notre Dame\n");
+                break;
+            case 14:
+                printf("“I was so upset, I forgot to be happy.” – Eeyore, Winnie the Pooh\n");
+                break;
+            case 15:
+                printf("“The flower that blooms in adversity is the most rare and beautiful of all.” – Emperor, Mulan\n");
+                break;
+            case 16:
+                printf("“Like so many things, it is not what’s on the outside, but inside that counts.” – Merchant, Aladdin\n");
+                break;
+            case 17:
+                printf("“Trust. It’s what friends do.” – Dory, Finding Nemo\n");
+                break;
+            case 18:
+                printf("“A lie keeps growing and growing until it’s as plain as the nose on your face.” – The Blue Fairy, Pinocchio\n");
+                break;
+            case 19:
+                printf("Not everyone can become a great artist, but a great artist can come from anywhere. – Anton Ego, Ratatouille\n");
+                break;
+            case 20:
+                printf("There is no greater gift than friendship. – Santa Claus, The Polar Express\n");
+                break;
+            case 21:
+                printf("Look inside yourself. You are more than what you have become. – Mufasa, The Lion King\n");
+                break;
+            case 22:
+                printf("Your identity is your most valuable possession. Protect it. – Elastigirl, The Incredibles\n");
+                break;
+            case 23:
+                printf("There is whole lot more to life than just winning. – Tex, Cars\n");
+                break;
+            case 24:
+                printf("To laugh at yourself, is to love yourself. – Mickey Mouse\n");
+                break;
+            case 25:
+                printf("All it takes is little faith and trust. – Peter Pan\n");
+                break;
+            case 26:
+                printf("Do not be fooled by commonplace appearance. Like so many things, it is not what outside, but what is inside that counts. – Aladdin\n");
+                break;
+            case 27:
+                printf("Your mind is like water. When it is agitated it becomes difficult to see, but when you let it settle; the answer becomes clear. – Oogway, Kung Fu Panda\n");
+                break;
+            case 28:
+                printf("I’m only brave enough when I have to be. Being brave doesn’t mean you go looking for trouble. – Mufasa, The Lion King\n");
+                break;
+            case 29:
+                printf("There are those who say fate is something beyond our command, that destiny is not our own. But I know better. Our fate lives within us. You only have to be brave enough to see it. – Merida, Brave\n");
+                break;
+            case 30:
+                printf("If you work on something long enough you will find it, even if you are lost for a while you will find it. – Professor Bomba, Epic\n");
+                break;
+            case 31:
+                printf("There is no secret ingredient. It’s just you. – Po, Kung Fu Panda\n");
+                break;
+            case 32:
+                printf("'You don’t have time to be timid. You must be bold and daring.' — Lumière, Beauty and the Beast\n");
+                break;
+            case 33:
+                printf("“First, think. Second, believe. Third, dream. And finally, dare.” – Walt Disney\n");
+                break;
+            case 34:
+                printf("“Don’t just fly, soar.” — Dumbo (Dumbo)\n");
+                break;
+            case 35:
+                printf("“You are braver than you believe, stronger than you seem, and smarter than you think.” — Christopher Robin, Winnie the Pooh\n");
+                break;
+            case 36:
+                printf(" “If you focus on what you left behind, you will never be able to see what lies ahead.” — Gusteau (Ratatouille)\n");
+                break;
+            case 37:
+                printf("“You must not let anyone define your limits because of where you come from. Your only limit is your soul.” – Ratatouille\n");
+                break;
+            case 38:
+                printf("“You control your destiny — you don’t need magic to do it. And there are no magical shortcuts to solving your problems.” — Merida (Brave)\n");
+                break;
+            case 39:
+                printf("“Always let your conscience be your guide.” — Jiminy Cricket, Pinocchio\n");
+                break;
+            case 40:
+                printf("“The way to get started is to quit talking and begin doing.” – Walt Disney\n");
+                break;
+            case 41:
+                printf("“The very things that hold you down are going to lift you up.” — Timothy Mouse, Dumbo\n");
+                break;
+            case 42:
+                printf(" “In every job that must be done, there is an element of fun. You find the fun and—snap!—the job’s a game!” – Mary Poppins\n");
+                break;
+            case 43:
+                printf("“Venture outside your comfort zone. The rewards are worth it.” – Rapunzel, Tangled\n");
+                break;
+            case 44:
+                printf("Open different doors, you may find a you there that you never knew was yours. Anything can happen.” – Mary Poppins.\n");
+                break;
+            case 45:
+                printf("“The only way to get what you want in this world is through hard work.” – Tiana, The Princess And the Frog.\n");
+                break;
+            case 46:
+                printf("“Ohana means family, and family means no one gets left behind or forgotten.” – Lilo & Stitch\n");
+                break;
+            case 47:
+                printf("“A true hero isn’t measured by the size of his strength, but by the strength of his heart.” — Zeus, Hercules\n");
+                break;
+            case 48:
+                printf("“When there’s too much to do, don’t let it bother you. Forget your troubles.” –Snow White, Snow White and the Seven Dwarfs\n");
+                break;
+            case 49:
+                printf("“Man has always learned from the past. After all, you can’t learn history in reverse!” – Archimedes, The Sword in the Stone\n");
+                break;
+            case 50:
+                printf("“The seaweed is always greener in somebody else’s lake.” – The Little Mermaid\n");
+                break;
+            case 51:
+                printf("“You’re only a fool if you give up.” – Aladdin\n");
+                break;
+            case 52:
+                printf("“If you cannot say something nice, don’t say nothing at all.” – Bambi\n");
+                break;
+            
+            default:
+                printf("Enter a valid number from the choices provided");
+        }
+}
